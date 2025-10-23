6ae6a04f2fa41e50420acff1c712ad322f3478a7
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2025-10-23 08:36:39 +0200

    Bump to v0.0.6

--- src/content_publisher/app/config.py
+++ src/content_publisher/app/config.py
@@ -64,7 +64,7 @@ class RedditPublisherConfig(PublisherConfig):
     @property
     def credentials(self) -> dict[str, Any]:
         app_id = "https://github.com/poshjosh/content-publisher"
-        app_version = "0.0.5"
+        app_version = "0.0.6"
         username = os.environ[f"{_PREFIX}_REDDIT_USERNAME"]
         return {
             'client_id': os.environ[f"{_PREFIX}_REDDIT_CLIENT_ID"],
