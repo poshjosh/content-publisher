8bd7c9ab2aa13c6044d105960b22bd75ee24ccb1
poshjosh <posh.bc@gmail.com>
2025-10-26 22:03:18 +0100

    Bump to v0.0.8

--- src/content_publisher/app/config.py
+++ src/content_publisher/app/config.py
@@ -59,7 +59,7 @@ class RedditPublisherConfig(PublisherConfig):
     @property
     def credentials(self) -> dict[str, Any]:
         app_id = "https://github.com/poshjosh/content-publisher"
-        app_version = "0.0.7"
+        app_version = "0.0.8"
         username = os.environ[f"{_PREFIX}_REDDIT_USERNAME"]
         return {
             'client_id': os.environ[f"{_PREFIX}_REDDIT_CLIENT_ID"],
